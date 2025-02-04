46a4cbae63890ab785b72025869f22a996c080bb
Satenik <s.tatevosyan01@gmail.com>
2025-02-04 17:10:12 +0400

    add User moudle to App

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import * as dotenv from 'dotenv';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 dotenv.config();
 
 @Module({
   imports: [
-	MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING!)
+	MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING!),
+	UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from "@nestjs/common";
+import { Get, Param } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Controller()
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
+
+	@Get('user/:email')
+	async getUserByEmail(@Param('email') email: string) {
+		return this.usersService.findByEmail(email);
+	}
 }
